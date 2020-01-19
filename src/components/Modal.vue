7e4420c2878d054b3f6bf8ce8bdf5985152eb1d8
djsz3y <294764484@qq.com>
2020-01-19 09:57:20 +0800

    2-4(1)2-5(2)父子组件传递以及插槽的讲解

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,29 +1,71 @@
 <template>
-  <div style="display: none;">
-    <div class="md-modal modal-msg md-modal-transition md-show">
+  <div><!-- 插槽16-1.style="display: none;"去掉默认打开。 -->
+		<!-- 插槽24.怎么用呢？md-show这个class了绑定class。【v-bind:class="{'md-show':mdShow}"】
+								然后就可以控制遮罩了。-->
+    <div class="md-modal modal-msg md-modal-transition" v-bind:class="{'md-show':mdShow}"><!-- 插槽16-2.把class里md-show去掉，关掉框 -->
       <div class="md-modal-inner">
         <div class="md-top">
-          <button class="md-close">关闭</button>
+					<!-- 插槽19-3.点击关闭按钮要绑定方法为closeModal的点击事件 -->
+          <button class="md-close" @click="closeModal">关闭</button>
         </div>
         <div class="md-content">
           <div class="confirm-tips">
-            <p slot="message">你确认要删除此条数据吗?</p>
+						<!-- 插槽1.动态添加一个弹框，动态传递数据。 -->
+            <!-- 插槽2.<p slot="message">你确认要删除此条数据吗?</p> -->
+						<!-- 插槽3.定义一个插槽，起个带名字的插槽 -->
+						<slot name="message"></slot>
           </div>
           <div class="btn-wrap">
-            <a slot="btnGroup" class="btn btn--m" href="javascript:;">确认</a>
-            <a slot="btnGroup" class="btn btn--m btn--red" href="javascript:;">关闭</a>
+						<!-- 插槽5.定义一个按钮组插槽 -->
+						<slot name="btnGroup"></slot>
+<!--            插槽6.<a slot="btnGroup" class="btn btn--m" href="javascript:;">确认</a>
+            <a slot="btnGroup" class="btn btn--m btn--red" href="javascript:;">关闭</a> -->
           </div>
         </div>
       </div>
     </div>
-    <div class="md-overlay"></div>
+		<!-- 插槽17.这是一个遮罩框。关掉弹框还有遮罩框md-overlay，所以这个也要一起控制。 -->
+    <!-- 插槽25-1.弹框已经生效了。默认mdShow是false -->
+		<!-- 插槽25-2.遮罩层的控制v-if="mdShow"
+								mdShow是true显示遮罩，是false不显示遮罩 -->
+								<!-- v-if v-show区别：没有DOM节点，有DOM节点，都控制元素渲染。
+										v-if判断是否加载，需要时加载，有更高的切换开销，减轻服务器压力。运行时条件很少改变，用v-if。
+										v-show调整css display属性，客户端操作更流畅，有更高的初始渲染开销。非常频繁的切换，用v-show。
+										不显示在视图上的话，这里用v-if渲染时不会渲染（无DOM节点），用v-show就是设置display为none（有DOM节点）。
+								-->
+		<div class="md-overlay" v-if="mdShow" @click="closeModal"></div>
+		<!-- 插槽26-1.怎么设置一般点叉关闭，点遮罩层关闭？
+								在遮罩层的这个div里也定义一个事件@click="closeModal"。
+		-->
   </div>
 </template>
 
 <script>
 export default {
   name: 'modal',
-  props: {
-  }
+	// 插槽18-1.怎样让组件和cart.vue的属性相结合？那就是mdShow这个属性那props:['mdShow']里就要用
+	// 插槽18-2.（props: ['mdShow'],）是一种简写。当然也可以用Object方式：
+				// props: {
+				// 	'mdShow':Boolean
+				// }
+	props: ['mdShow'],
+	// 插槽19-1.很明显要调用方法。任意一个组件都是有methods的。
+	// 插槽19-2.编写方法closeModal，上面绑定点击事件了。
+	// 插槽19-3.点击关闭按钮要绑定方法为closeModal的点击事件
+							// <button class="md-close" @click="closeModal">关闭</button>
+	methods:{
+		closeModal(){
+			// 插槽20-1.不在里面直接关掉,传递父组件来关闭。调用父组件。
+			// 插槽20-2.<modal :mdShow="modalConfirm">是子组件触发父组件的一个事件modalConfirm
+			// 插槽20-3.怎么通过子组件触发父组件的一个事件？记住父子组件之间传递是通过this.$emit()触发这个事件。
+			this.$emit('close');//即触发父组件的close事件的方法。
+			/**插槽26-2.
+			 * 重要！！！
+			 * 不能写这个this.mdShow = false;这样会只能点击一次删除图标的弹框然后点叉号关闭后再也点不了。
+			 * 不管开,还是关【this.$emit('close');】,一定通过父组件控制的
+			 */
+			
+		}
+	}
 }
 </script>
--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -91,7 +91,9 @@
               </div>
               <div class="cart-tab-5">
                 <div class="cart-item-opration">
-                  <a href="javascript:;" class="item-edit-btn">
+									<!-- 插槽11.删除图标打开弹框，删除图标点击后删除,根据id删掉，所以要传参，把item对象传过来
+															所以要在底下定义一个delCartConfirm方法-->
+                  <a href="javascript:;" class="item-edit-btn" @click="delCartConfirm(item)">
                     <svg class="icon icon-del">
                       <use xlink:href="#icon-del"></use>
                     </svg>
@@ -130,8 +132,35 @@
   <!-- 标签的方式用nav-footer组件 -->
   <nav-footer></nav-footer>
 	<!-- 标签的方式用modal组件 -->
-	<modal>
-		
+	<!-- 插槽8.绑定事件显示弹框，一般用v-bind绑定事件，v-bind:任何属性可绑（src）
+						【v-bind:属性】 可以简写成冒号属性即 【:属性】
+						【v-on可简写@】 如【v-on:click => @click】 -->
+	<!-- 插槽9.属性里一定有监听对象，所以modal一定要定义好，在下面data(){}里。 -->
+	<!-- 插槽21.父组件怎么接收？在cart.vue里，父组件添加自定义事件@close="closeModal"。
+							这个close是this.$emit('close'); -->
+	<modal :mdShow="modalConfirm" @close="closeModal">
+		<!-- 插槽28-1.新语法用template模板(官方)：<template v-slot:message></template>不能用v-slot="message"
+								所有插槽都用template模板包一层。
+		-->
+		<template v-slot:message>
+			<p>你确认要删除此条数据吗?</p>
+		</template>
+		<!-- 插槽4.使用插槽message：
+							<p slot="message">你确认要删除此条数据吗?</p> -->
+
+		<!-- 插槽28-2.新语法用template模板包一层，也没有div了 -->
+		<template v-slot:btnGroup>
+		<!-- 插槽7.使用按钮组插槽，一般用div框起来，给div一个插槽，a标签里slot属性就去掉了 -->
+		<!-- <div slot="btnGroup"> -->
+			<!-- 插槽29.delCart：彻底的删掉数据。 -->
+			<a class="btn btn--m" href="javascript:;" @click="delCart">确认</a>
+			<!-- 插槽27.这里是@click，不是自定义事件（自定义事件是因为从Modal.vue关联到@close，不然难道触发所有@click吗，显然不可能）了，可以触发modalConfirm
+									@click有两种写法：一种是调用方法closeModal，一种是表达式modalConfirm=false。
+									vue的事件里不仅可以写方法，也可以写逻辑表达式。
+			-->
+			<a class="btn btn--m btn--red" href="javascript:;" @click="modalConfirm=false">关闭</a>
+		<!-- </div> -->
+		</template>
 	</modal>
 </div>
 </template>
@@ -148,6 +177,8 @@ export default {
 	// 如果直接暴露一个object，就变成了一个全局的data变量了。
 	data(){
 		return{
+			modalConfirm:false,//插槽10.弹框显示属性。默认false，什么时打开呢，点删除按钮的时候。
+			delItem:'',//插槽15-2.准备删除的对象
 			cartList:[]
 		}
 	},
@@ -207,7 +238,41 @@ export default {
 				item.checked = !item.checked;
 				//清除一下缓存
 			}
-		}
+		},
+		// 插槽12.删除数据,确认弹框
+		delCartConfirm(item){
+			// 插槽14.删除真的要删,所以要先把这个删除的对象保存下来
+			// 插槽15-1.delItem是个变量,所有的变量都要定义,所以在上面data(){}里定义一下。
+			this.delItem = item;
+			// 插槽13.点击时确认是否删除,所以先把对象放开。
+			this.modalConfirm = true;//点击时对象设为true。
+			
+		},
+		// 插槽22.定义关闭弹框的方法。
+		closeModal(){
+			this.modalConfirm = false;//关闭时通过父组件关掉。
+			// 插槽23.总结:关掉的对象会传到<modal :mdShow里,传递的属性叫mdShow,跑到Modal.vue的props:['mdShow']
+			// 插槽24.怎么用呢？md-show这个class了绑定class。
+		},
+		/**
+		 * 插槽30.删除购物车数据。
+		 * 删除之前购物车的对象已经保存出来了：见插槽14.this.delItem = item;。
+		 */
+		delCart(){
+			// 先获取一下item
+			let delItem = this.delItem;
+			//渲染是用v-for循环cartList渲染上去的。把cartList干掉就可以了。
+			this.cartList.forEach((item,index)=>{
+				if(delItem.productId == item.productId){
+					// splice 2-5 9:47
+					this.cartList.splice(index,1);
+					this.modalConfirm = false;
+				}
+			})
+			// 好用神奇数组方法
+		},
+		
 	}
+	
 }
 </script>
